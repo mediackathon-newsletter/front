eb49df9c36b6e96799102000efd7cab4d0fe1707
leo-malassagne <malassagne.leo@gmail.com>
2018-04-28 17:10:14 +0200

    Add profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,42 @@ const Profile = () => {
     <section className="section profile">
       <div className="box">
         <h1 className="title is-2">Preferences</h1>
+        <div className="columns">
+          <div className="column">
+            <h2 className="subtitle is-3">Profil</h2>
+            <div className="field is-grouped">
+              <label className="label">Nom </label>
+              <div className="control">
+                <input className="input" type="text"/>
+              </div>
+              <label className="label">Prénom </label>
+              <div className="control">
+                <input className="input" type="text"/>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input className="input" type="text"/>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Age</label>
+              <div className="control">
+                <input className="input" type="number"/>
+              </div>
+            </div>
+            <button className="button is-link">Editer</button>
+          </div>
+          <div className="column">
+            <h2 className="subtitle is-3">Abonnement</h2>
+            <div className="message is-info">
+              <div className="message-header">
+                <p>Votre abonnement actuel</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
