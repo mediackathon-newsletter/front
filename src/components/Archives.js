b7d51685458e15d3b3fd2aaba5dd35de902d6f2e
guilum <38065849+guilum@users.noreply.github.com>
2018-04-28 17:09:52 +0200

    modif sur ideas & archives

--- src/components/Archives.js
+++ src/components/Archives.js
@@ -5,7 +5,44 @@ const Archives = () => {
     <section className="section archives">
       <div className="box">
         <h1 className="title is-2">Archives</h1>
-      </div>
+      
+	  
+	  <div className="container">
+		<ul>
+			<li className="container">
+				<a href="#" className="button">						
+					<span className="">
+						30 avril 2018 - 
+					</span>
+					<span className="">
+						Titre de la newsletter 8
+					</span>
+				</a>
+			</li>
+			<li className="">
+				<label className="">
+					<span className="">
+						23 avril 2018  -
+					</span>
+					<span className="">
+						Titre de la newsletter 7
+					</span>
+				</label>
+			</li>
+			<li className="">
+				<label className="">
+					<span className="">
+						16 avril 2018 - 
+					</span>
+					<span className="">
+						Titre de la newsletter 6
+					</span>
+				</label>
+			</li>
+		</ul>
+	  </div>
+	  </div>
+	  
     </section>
   );
 };
--- src/components/Idea.js
+++ src/components/Idea.js
@@ -5,8 +5,26 @@ const Idea = () => {
     <section className="section idea">
       <div className="box">
         <h1 className="title is-2">Boite à idées</h1>
-      </div>
+
+			<form method="" className="">
+				<div className="field">
+					<label className="label">
+						Soumettez des idées de sujet sur votre quartier et 
+						gagnez des [points Membre] si votre sujet est choisi. 
+					</label>
+					<div className="control">
+						<textarea className="textarea" placeholder="Saisissez vos suggestions"></textarea>
+					</div>
+				</div>
+				<div className="field is-grouped">
+					<div className="control">
+						<button className="button is-link">Envoyer</button>
+					</div>
+				</div>
+			</form>
+		</div>
     </section>
+	
   );
 };
 
